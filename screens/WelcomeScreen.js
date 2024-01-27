482bc88b31f3a7fc39d276efbd95a62fa1b43f9f
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-01-27 01:43:18 +0100

    welcome screen

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,12 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
 
-export default function WelcomeScreen() {
+function WelcomeScreen() {
   return (
-    <View>
-      <Text>WelcomeScreen</Text>
+    <View style={styles.rootContainer}>
+      <Text style={styles.title}>Welcome!</Text>
+      <Text>You authenticated successfully!</Text>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 32,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+});
