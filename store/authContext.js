971a29b54e26a8370efe1c2fd0ef86769cb771a3
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-01-27 15:33:38 +0100

    Set token and get token to and from the device

--- store/authContext.js
+++ store/authContext.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const AuthContext = createContext({
   token: "",
@@ -11,8 +12,19 @@ function AuthContentProvider({ children }) {
   const [authToken, setAuthToken] = useState();
   // const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+  useEffect(() => {
+    async function fetchToken() {
+      const sharedToken = await AsyncStorage.getItem("token");
+      if (sharedToken) {
+        setAuthToken(sharedToken);
+      }
+    }
+    fetchToken();
+  }, []);
+
   function authenticate(token) {
     setAuthToken(token);
+    AsyncStorage.setItem("token", token);
     // setIsAuthenticated(true);
   }
   function logout() {
