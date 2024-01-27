a6e7f664d7d0b2ec6c8c389b4fdbdaead672cca6
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-01-27 01:42:51 +0100

    Sign up screen

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,12 +1,7 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import AuthContent from "../components/Auth/AuthContent";
 
-export default function SignupScreen() {
-  return (
-    <View>
-      <Text>SignupScreen</Text>
-    </View>
-  )
+function SignupScreen() {
+  return <AuthContent />;
 }
 
-const styles = StyleSheet.create({})
+export default SignupScreen;
